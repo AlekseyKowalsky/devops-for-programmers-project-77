8f8077ee369f0b09ec6f2a6f5d7f2b14d8513173
Aleksei Khrapov <aleksei.khrapov@videoly.co>
2023-12-15 17:57:34 +0200

    feat: change acme endpoint to stage

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -20,7 +20,7 @@ provider "digitalocean" {
 }
 
 provider "acme" {
-  server_url = "https://acme-v02.api.letsencrypt.org/directory"
+  server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
 }
 
 provider "tls" {}
