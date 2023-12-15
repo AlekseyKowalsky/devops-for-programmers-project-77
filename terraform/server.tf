e23f485dc428e238617c33a2442fd1a8fa4be55f
Aleksei Khrapov <aleksei.khrapov@videoly.co>
2023-12-15 17:57:45 +0200

    feat: add ssh keys to servers

--- terraform/server.tf
+++ terraform/server.tf
@@ -1,8 +1,14 @@
+data "digitalocean_ssh_key" "key" {
+  name = "Mac Pro 13"
+}
+
 resource "digitalocean_droplet" "web-1" {
   image  = "docker-20-04"
   name   = "tfhexlet-1"
   region = "fra1"
   size   = "s-1vcpu-1gb"
+
+  ssh_keys = [data.digitalocean_ssh_key.key.id]
 }
 
 resource "digitalocean_droplet" "web-2" {
@@ -10,12 +16,14 @@ resource "digitalocean_droplet" "web-2" {
   name   = "tfhexlet-2"
   region = "fra1"
   size   = "s-1vcpu-1gb"
+
+  ssh_keys = [data.digitalocean_ssh_key.key.id]
 }
 
-output "web-1-ip" {
-  value = digitalocean_droplet.web-1.ipv4_address
-}
+resource "null_resource" "gen_yaml" {
+  depends_on = [digitalocean_droplet.web-1, digitalocean_droplet.web-1]
 
-output "web-2-ip" {
-  value = digitalocean_droplet.web-2.ipv4_address
+  provisioner "local-exec" {
+    command = "echo 'server_1_ip: ${digitalocean_droplet.web-1.ipv4_address}\nserver_2_ip: ${digitalocean_droplet.web-2.ipv4_address}' > ${var.path_to_store_server_ips}"
+  }
 }
