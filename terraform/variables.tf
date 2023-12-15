a09b0456522002b7794ffd92710fc285e460df0e
Aleksei Khrapov <aleksei.khrapov@videoly.co>
2023-12-15 17:58:06 +0200

    fix: vars +

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,9 @@ variable "do_token" {
   description = "Access token to Digital Ocean platform"
   sensitive = true
 }
+
+variable "path_to_store_server_ips" {
+  description = "Path to a yaml file keeping IPs addresses of created servers"
+  default = "../ansible/group_vars/all/server-ips.yaml"
+  sensitive = false
+}
