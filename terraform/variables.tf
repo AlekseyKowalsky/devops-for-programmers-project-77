23e14763be7a18b737446b967a74eedf74d76d8e
Aleksei Khrapov <aleksei.khrapov@videoly.co>
2023-12-19 09:07:28 +0000

    fix: add types to vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,36 +1,42 @@
 variable "do_token" {
+  type        = string
   description = "Access token to Digital Ocean platform"
   sensitive   = true
 }
 
 variable "datadog_api_key" {
+  type        = string
   description = "API key of Datadog"
   sensitive   = true
 }
 
 variable "datadog_app_key" {
+  type        = string
   description = "App key of Datadog"
   sensitive   = true
 }
 
-
 variable "path_to_store_server_ips" {
+  type        = string
   description = "Path to a yaml file keeping IPs addresses of created servers"
   default     = "../ansible/group_vars/all/server-ips.yaml"
   sensitive   = false
 }
 
 variable "admin_email" {
+  type        = string
   description = "Email to receive alerts and create certificates"
   default     = "al.kowalsky7@gmail.com"
 }
 
 variable "domain_name" {
+  type        = string
   description = "General domain name managing by cloud provider"
   default     = "alekspaces.com"
 }
 
 variable "app_port" {
+  type        = number
   description = "App port for forwarding traffic throughout the balancer to droplets"
   default     = 3000
 }
